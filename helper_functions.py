9609488fdd3d119f5199076d1578db05ed80254c
falakrastogi <falakrast1@gmail.com>
2024-05-17 20:56:39 +0530

    code

--- helper_functions.py
+++ helper_functions.py
@@ -1,13 +1,18 @@
 """"File with all helper functions"""
 import os
 
+colours_dictionary = {
+    'red': "\033[91m",  # \033 is an ansi escape code and 91m refers to red.
+    'base': "\033[0m"  # the base colour is white. Again, \033 is the escape code and 0m stands for default i.e. white
+}
+
 
 # os.curdir -> gives the current directory which would be usually just "."
 # os.path.join -> will join two paths together.
 # os.listdir -> gives the name of all files present in the argument directory.
 
 
-def get_files_path(extension) -> []:
+def get_files_path(extension='.txt') -> []:
     """"Returns a list of all files of the given extension with full paths."""
 
     file_path = os.path.join(os.curdir, 'words_directory')  # file_path = ./words_directory
@@ -27,4 +32,5 @@ def find_word(word_to_find=None):
             # lines in that file.
             for line in lines:
                 if word_to_find in line:
-                    print(line)
+                    new_line = line.replace(word_to_find, f"{colours_dictionary['red']}{word_to_find}{colours_dictionary['base']}")
+                    print(f"{file}:{new_line}")
